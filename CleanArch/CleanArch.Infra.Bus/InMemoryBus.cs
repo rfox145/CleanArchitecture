229fda3bab5c96ca90eb105cbe3128fac41e5ad0
Hector <hector.erc@gmail.com>
2021-09-24 08:40:56 -0400

    Adding Infra.Bus Project and MediatR library

---
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Bus
+{
+   public sealed class InMemoryBus : IMediator
+    {
+        private readonly IMediator mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public Task Publish(object notification, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> Send(object request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
